ff52de645749c335c1ce508ebcb4e95e0a64d693
Sagar Ali <sagar18.iosdeveloper@gmail.com>
2020-08-20 20:35:55 +0500

    Initial commit

---
+++ app/src/main/java/com/example/accelometer_sensor_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.accelometer_sensor_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
